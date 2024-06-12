c556518520b48a95ee0150d42cf564a21a15fe96
ILzat <ilzat.sadykov.2006@bk.ru>
2024-06-13 01:39:30 +0600

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import MovieInput from './Components/MovieInput/MovieInput.tsx';
+
+const App = () => (
+    <>
+<MovieInput/>
+    </>
+);
+
+export default App
---
+++ src/Components/MovieInput/MovieInput.tsx
@@ -0,0 +1,12 @@
+const MovieInput = () => {
+  return (
+    <>
+     <input type={"text"}
+     >
+
+     </input>
+    </>
+  );
+};
+
+export default MovieInput;

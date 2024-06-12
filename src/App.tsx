330860813f73193ae7808ef27a59e37fca953062
ILzat <ilzat.sadykov.2006@bk.ru>
2024-06-13 02:09:15 +0600

    'Сделал input'

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import './App.css'
+import './App.css';
 import MovieInput from './Components/MovieInput/MovieInput.tsx';
+import React, {useState} from 'react';
 
-const App = () => (
+const App = () => {
+  const [MovieInp, setMovieInp] = useState('');
+  console.log(MovieInp);
+  const changeMovie = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMovieInp(e.target.value);
+  };
+  return (
     <>
-<MovieInput/>
+      <MovieInput value={MovieInp} onChange={changeMovie}/>
     </>
-);
+  );
+};
 
-export default App
+export default App;
--- src/Components/MovieInput/MovieInput.tsx
+++ src/Components/MovieInput/MovieInput.tsx
@@ -1,10 +1,20 @@
-const MovieInput = () => {
+import React from 'react';
+
+interface Props {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const MovieInput: React.FC<Props> = ({value, onChange}) => {
   return (
     <>
-     <input type={"text"}
-     >
-
-     </input>
+      <h4>Add Movie</h4>
+      <input
+        type={'text'}
+        value={value}
+        onChange={onChange}
+      />
+      <button type={'submit'}>Add</button>
     </>
   );
 };
